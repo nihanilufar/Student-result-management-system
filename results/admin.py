a40f1170390d26c7a03072bbb70e69ddf3f6c9ac
nihanilufar <nihanilufar00@gmail.com>
2026-06-26 21:34:48 +0530

    Updated models and admin

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import student,Subject,Result
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list display = ['name','roll_number','batch']
+    search_fields = ['name','roll_number']
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list display = ['name','code','max_marks']
+
+@admin.register(result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['student','subject','marks_obtained']
+    list_filter = ['subject','student__batch']
+
+
+
--- results/models.py
+++ results/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 
 class student(models.Model):
-    
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=15,unique=True)
+    batch = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} ({self.roll_number})"
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=15,unique=True)
+    max_marks = models.Integration(default=100)
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    student = models.Foreignkey(student,on_delete=models.CASCADE,related_name='results')
+    subject = models.Foreignkey(Subject,on_delete=models.CASCADE)
+    marks_obtained = models.IntegerField()
+    exam_date = models.DateField(auto_now_add=True)
+
+    class meta:
+        unique_together = ('student','subjects')
+
+
+    def __str__(self):
+        return f"{self.student} : {self.subject}"
+
+
+
+                     
 
